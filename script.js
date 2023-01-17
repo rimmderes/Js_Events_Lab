d1b294e33bf247062f1c19e42e954ad26af4ea83
rimmderes <rimmderes@gmail.com>
2023-01-17 16:27:43 +0000

    added show date button and completed mvp

--- script.js
+++ script.js
@@ -2,23 +2,23 @@ const enter = document.querySelector("#enter");
 
 const listItemForm = document.querySelector("#listItem")
 
+// FORM
 
 const handleToDoForm = (event) => {
     
     event.preventDefault();
-    // console.log("test");
+    
     const toDoItem = document.createElement("li");
     toDoItem.textContent = event.target.toDo.value;
     
     const itemDelete = document.createElement("button");
-    // const existingDelete = document.querySelector("#delete");
+    
     itemDelete.textContent = "Delete me!";
     itemDelete.onclick = function(){
         this.parentElement.removeChild(toDoItem);
         this.parentElement.removeChild(itemDelete);
     }
     
-    // event.target.("button").remove();
 
     console.log(toDoItem.textContent);
     console.log(itemDelete.textContent);
@@ -32,6 +32,13 @@ const handleToDoForm = (event) => {
 
 listItemForm.addEventListener("submit", handleToDoForm);
 
-// const deleteThisToDo = () => {
-//     itemDelete.
-// };
+// DATE 
+
+const dateButton = document.querySelector("#date");
+
+dateButton.onclick = (event) => {
+    const dateToday = new Date();
+    document.getElementById("datePara").textContent = datetoday;
+    console.log(dateToday);
+}
+
